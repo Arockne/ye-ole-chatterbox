98684652ec4930c3cb1fc46201fb548c5d5436c6
Daniel Calvin <arockne2003@yahoo.com>
2022-01-01 15:10:19 -0700

    implement logout through removing user id from session

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,4 +9,9 @@ class Api::SessionsController < ApplicationController
       render json: { error: 'Invalid username or password'}, status: :unauthorized
     end
   end
+
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
 end
