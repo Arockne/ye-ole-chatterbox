fd68a7144bd533669ff745c09ea8a27cf370b76f
Daniel Calvin <arockne2003@yahoo.com>
2022-01-01 16:37:04 -0700

    testing authorization

--- app/controllers/api/hello_controller.rb
+++ app/controllers/api/hello_controller.rb
@@ -1,4 +1,6 @@
 class Api::HelloController < ApplicationController
+  skip_before_action :authorize, only: [:hello_world]
+  
   def hello_world
     session[:count] = (session[:count] || 0) + 1
     render json: { count: session[:count] }, status: :ok
