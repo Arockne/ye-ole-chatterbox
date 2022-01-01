975738e51ab2fdba8d62b6e651aa16d88af33974
Daniel Calvin <arockne2003@yahoo.com>
2022-01-01 16:53:30 -0700

    move App into components directory

---
+++ client/src/components/App.js
@@ -0,0 +1,26 @@
+// client/src/components/App.js
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/hello" element={<h1>Test Route</h1>}/>
+          <Route path="/" element={<h1>Page Count: {count}</h1>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
