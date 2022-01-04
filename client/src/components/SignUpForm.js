ed3ad591d408fbb9791c9c4d487b7f3293d1c4c6
Daniel Calvin <arockne2003@yahoo.com>
2022-01-03 17:11:12 -0700

    rename SignUp component to SignUpForm

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function SignUp() {
+function SignUpForm() {
   return (
     <div>I am the sign up component!</div>
   )
 }
 
-export default SignUp
+export default SignUpForm
