e89ea0808483735357ff0b32d44389521ed55d09
Daniel Calvin <arockne2003@yahoo.com>
2022-01-04 11:45:03 -0700

    implement error handling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -8,15 +8,38 @@ const defaultLoginFormData = {
 
 function LoginForm() {
   const [formData, setFormData] = useState(defaultLoginFormData)
+  const [errors, setErrors] = useState([])
 
   function handleFormChange(e) {
     const { name, value } = e.target
     setFormData(formData => ({ ...formData, [name]: value }) ) 
   }
-  
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(r => {
+      if (r.ok) {
+        r.json().then(user => {
+
+        })
+      } else {
+        r.json().then(({errors}) => {
+          setErrors(errors)
+        })
+      }
+    })
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <label>Username:</label>
         <input 
           type='text'
@@ -34,6 +57,9 @@ function LoginForm() {
         />
         <input type='submit'/>
       </form>
+      {
+        errors.map(error => <p key={error}>{error}</p>)
+      }
       <Link to='/signup'>To Signup</Link>
     </div>
   )
