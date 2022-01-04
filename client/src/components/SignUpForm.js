b6c58ef5ffbb632ecd1ec48c350ee1b92e4936cb
Daniel Calvin <arockne2003@yahoo.com>
2022-01-03 18:40:14 -0700

    implement controlled form

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+const signUpFormDefaultValues = {
+  username: '',
+  password: '',
+  password_confirmation: ''
+}
+
 function SignUpForm() {
+  const [formData, setFormData] = useState(signUpFormDefaultValues)
+
+  function handleFormChange(e) {
+    const { name, value } = e.target
+    setFormData(formData => ({ ...formData, [name]: value }))
+  }
+
   return (
     <div>
       <form>
         <label>Username:</label>
-        <input />
+        <input 
+          type='text'
+          name='username'
+          value={formData.username}
+          onChange={handleFormChange}
+        />
         <label>Password:</label>
-        <input />
+        <input 
+          type='password'
+          autoComplete='off'
+          name='password'
+          value={formData.password}
+          onChange={handleFormChange}
+        />
         <label>Password Confirmation:</label>
-        <input />
+        <input 
+          type='password'
+          autoComplete='off'
+          name='password_confirmation'
+          value={formData.password_confirmation}
+          onChange={handleFormChange}
+        />
         <input type='submit'/>
       </form>
       <Link to='/'>To Login</Link>
