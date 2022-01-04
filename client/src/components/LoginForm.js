77aaa494ee765a57d17cb07389e70ec46db64e2b
Daniel Calvin <arockne2003@yahoo.com>
2022-01-03 18:27:29 -0700

    implement controlled form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
+
+const defaultLoginFormData = {
+  username: '',
+  password: ''
+}
+
 function LoginForm() {
+  const [formData, setFormData] = useState(defaultLoginFormData)
+
+  function handleFormChange(e) {
+    const { name, value } = e.target
+    setFormData(formData => ({ ...formData, [name]: value }) ) 
+  }
+  
   return (
     <div>
       <form>
         <label>Username:</label>
-        <input />
+        <input 
+          type='text'
+          name='username'
+          value={formData.username}
+          onChange={handleFormChange}
+        />
         <label>Password:</label>
-        <input />
+        <input 
+          type='password'
+          autoComplete='off'
+          name='password'
+          value={formData.password}
+          onChange={handleFormChange}
+        />
         <input type='submit'/>
       </form>
       <Link to='/signup'>To Signup</Link>
