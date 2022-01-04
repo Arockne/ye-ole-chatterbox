41388abad21c067cd725a25c25009be75d2c3fe7
Daniel Calvin <arockne2003@yahoo.com>
2022-01-04 14:38:18 -0700

    implement exception handling for invalid data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   before_action :authorize
 
   private
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
   def current_user
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
