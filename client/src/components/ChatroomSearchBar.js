fa8e8529438593065fce860e48b2d6f48519e39e
Daniel Calvin <arockne2003@yahoo.com>
2022-01-06 16:42:55 -0700

    implement controlled radio buttons

--- client/src/components/ChatroomSearchBar.js
+++ client/src/components/ChatroomSearchBar.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ChatroomSearchBar() {
+  const [sortBy, setSortBy] = useState('name')
+
+  function handleRadioChange(e) {
+    setSortBy(sortBy => e.target.value)
+  }
+  
   return (
-    <div></div>
+    <div>
+      <input type='text'/>
+      <div>
+        <fieldset>
+          <label>
+            <input 
+              type='radio' 
+              name='sort' 
+              value='name'
+              checked={sortBy === 'name'} 
+              onChange={handleRadioChange}
+            />
+            By Name
+          </label>
+          <label>
+            <input 
+              type='radio' 
+              name='sort' 
+              value='user' 
+              checked={sortBy === 'user'}
+              onChange={handleRadioChange}
+            /> 
+            By User
+          </label>
+        </fieldset>
+      </div>
+    </div>
   )
 }
 
