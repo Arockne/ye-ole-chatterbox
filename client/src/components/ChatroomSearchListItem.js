7d1d2e0eb73d57460779e06b5c90a73a9f58a12e
Daniel Calvin <arockne2003@yahoo.com>
2022-01-07 12:00:08 -0700

    added structure and styling

---
+++ client/src/components/ChatroomSearchListItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../images/moustache-man.jpg'
+
+
+function ChatroomSearchListItem({ chatroom }) {
+  const { name, image_url, bio } = chatroom
+  return (
+    <div className='search-item'>
+      <img src={image_url || logo} alt={image_url === '' ? 'moustache man' : 'chatroom image'} width='90rem'/>
+      <h3>{name}</h3>
+      <p>{bio}</p>
+    </div>
+  )
+}
+
+export default ChatroomSearchListItem
