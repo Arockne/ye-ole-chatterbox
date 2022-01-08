bcccc61ef49c29997fd0afea50f7db497d05c07f
Daniel Calvin <arockne2003@yahoo.com>
2022-01-08 12:49:30 -0700

    remove chatroom_members table, model, and migration

--- app/models/chatroom_member.rb
+++
@@ -1,4 +0,0 @@
-class ChatroomMember < ApplicationRecord
-  belongs_to :user
-  belongs_to :chatroom
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_173617) do
+ActiveRecord::Schema.define(version: 2022_01_08_164718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "chatroom_members", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "chatroom_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["chatroom_id"], name: "index_chatroom_members_on_chatroom_id"
-    t.index ["user_id"], name: "index_chatroom_members_on_user_id"
-  end
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
@@ -40,6 +31,4 @@ ActiveRecord::Schema.define(version: 2022_01_08_173617) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "chatroom_members", "chatrooms"
-  add_foreign_key "chatroom_members", "users"
 end
