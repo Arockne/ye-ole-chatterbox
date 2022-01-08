fa7eb3b4a19050308349380048e227bfa50101ff
Daniel Calvin <arockne2003@yahoo.com>
2022-01-08 13:01:13 -0700

    implement association macros

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,2 +1,4 @@
 class Chatroom < ApplicationRecord
+  has_many :chatroom_memberships
+  has_many :members, through: :chatroom_memberships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :chatroom_memberships
+  has_many :chatrooms, through: :chatroom_memberships
+  
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
   has_secure_password
