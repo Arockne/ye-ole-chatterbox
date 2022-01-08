cb87ee5b74d7d8c2d29a7c9141c37296ce940bf8
Daniel Calvin <arockne2003@yahoo.com>
2022-01-08 09:42:27 -0700

    migrate chatrooms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_234801) do
+ActiveRecord::Schema.define(version: 2022_01_08_164101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
