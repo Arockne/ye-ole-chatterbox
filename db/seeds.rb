c5ee94ef5546afcc163b8e1700814fae86dde5a0
Daniel Calvin <arockne2003@yahoo.com>
2022-01-08 09:53:04 -0700

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Chatroom.create ([
+  {
+    name: 'Jolly Good Chatter',
+    image_url: '',
+    bio: 'All about the chatting'
+  },
+  {
+    name: 'Crumpets and Tea',
+    image_url: '',
+    bio: 'Have some wondrous tea with some mighty fine chatting'
+  },
+  {
+    name: 'Polo Club',
+    image_url: '',
+    bio: 'The most exclusive chat about the sport of polo'
+  },
+  {
+    name: 'Art Gallery',
+    image_url: '',
+    bio: 'Most luxurious form of chatting about fine art'
+  },
+  {
+    name: 'Hello, is it me you looking for?',
+    image_url: '',
+    bio: 'I can see it in your eyes, I can see it in your smile.'
+  }
+])
