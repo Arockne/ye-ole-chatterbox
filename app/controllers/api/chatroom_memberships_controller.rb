a00b0f5bc970f27c909223e256ca54fb08dc2a51
Daniel Calvin <arockne2003@yahoo.com>
2022-01-08 13:39:34 -0700

    implement not found error when chatroom does not exist

--- app/controllers/api/chatroom_memberships_controller.rb
+++ app/controllers/api/chatroom_memberships_controller.rb
@@ -1,9 +1,16 @@
 class Api::ChatroomMembershipsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def create
     chatroom = Chatroom.find(params[:chatroom_id])
     chatroom.chatroom_memberships.create(user: @current_user)
     render json: chatroom, status: :created
+  end
+
+  private
+
+  def render_not_found
+    render json: { errors: ['Chatroom does not exist']}, status: :not_found
   end
 
 end
