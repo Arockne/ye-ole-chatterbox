a0eaa036d46aacafdb7aff6c47bb3e2549789ab8
Daniel Calvin <arockne2003@yahoo.com>
2022-01-12 08:52:34 -0700

    add association macros

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -1,4 +1,4 @@
 class Communication < ApplicationRecord
-  belongs_to :chatroom_membership
+  belongs_to :user
   belongs_to :chatroom
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :chatroom_memberships
   has_many :memberships, through: :chatroom_memberships, source: :chatroom
+  has_many :communications
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
