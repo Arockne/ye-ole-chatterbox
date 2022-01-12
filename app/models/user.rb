7bb93d6484fb3632e82e70f154537e41572b28ec
Daniel Calvin <arockne2003@yahoo.com>
2022-01-12 12:40:07 -0700

    all data dependent on user will be destroyed if user get destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :chatroom_memberships
+  has_many :chatroom_memberships, dependent: :destroy
   has_many :memberships, through: :chatroom_memberships, source: :chatroom
-  has_many :communications
+  has_many :communications, dependent: :destroy
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
