b2ac8150302c47278a391ee6ec31e696b3ff98d4
Daniel Calvin <arockne2003@yahoo.com>
2022-01-12 10:51:18 -0700

    implement request for chatroom

--- client/src/components/Chatroom.js
+++ client/src/components/Chatroom.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import ChatroomHeader from './ChatroomHeader'
 import Messages from './Messages'
 import Members from './Members'
 
 function Chatroom() {
+  const [chatroom, setChatroom] = useState({})
+  const [errors, setErrors] = useState([])
+  const {id} = useParams()
+
+
+  useEffect(() => {
+    fetch(`/api/chatrooms/${id}`)
+    .then(r => {
+      if (r.ok) {
+        r.json().then(data => {
+          setChatroom(data)
+        })
+      } else {
+        r.json().then(e => setErrors(e.errors))
+      }
+    })
+  }, [id])
+
   return (
     <div>
       <ChatroomHeader />
