601e992f6593e7653d1cf26a59ce182f8efb8de0
Daniel Calvin <arockne2003@yahoo.com>
2022-01-12 15:47:42 -0700

    only allow messages that are not empty

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -2,6 +2,8 @@ class Communication < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 
+  validates :message, presence: true
+
   def creator
     { id: user.id, username: user.username, image_url: user.image_url }
   end
