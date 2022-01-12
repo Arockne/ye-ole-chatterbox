3310b8a815164bb9a390eec99b539e9292c2e80d
Daniel Calvin <arockne2003@yahoo.com>
2022-01-12 08:50:32 -0700

    commented out possible future implementation

--- app/controllers/api/chatrooms_controller.rb
+++ app/controllers/api/chatrooms_controller.rb
@@ -1,4 +1,6 @@
 class Api::ChatroomsController < ApplicationController
+  # before_action :chatroom_authorization
+  # skip_before_action :chatroom_authorization, only: [:index]
 
   def index
     chatrooms = Chatroom.all
@@ -9,5 +11,15 @@ class Api::ChatroomsController < ApplicationController
     chatroom = Chatroom.find(params[:id])
     render json: chatroom, status: :ok, serializer: ChatroomShowSerializer
   end 
+
+  # private
+
+  # def is_member?
+  #   chatroom.find(params[:id]).members.include? @user
+  # end
+
+  # def chatroom_authorization
+  #   render json: { errors: ["Not authorized"]}, status: :unauthorized unless is_member?
+  # end
 
 end
