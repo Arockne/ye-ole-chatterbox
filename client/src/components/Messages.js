8cc2397e87c787d3ff478f91fcb71dffdad01237
Daniel Calvin <arockne2003@yahoo.com>
2022-01-14 13:06:56 -0700

    implement scrolling to bottom when user sends a message

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Message from './Message'
 
 function Messages({ communications }) {
+  const messageEndRef = useRef(null)
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+  }, [communications])
+
   return (
     <div className='messages'>
       {
         communications.map(communication => <Message key={communication.id} communication={communication}/>)
       }
+      <div ref={messageEndRef}/>
     </div>
   )
 }
