06b26d096664b8cbb19c9844c871fbe7a68c4c34
Daniel Calvin <arockne2003@yahoo.com>
2022-01-14 10:18:22 -0700

    add colors

--- client/src/components/MembershipNav.js
+++ client/src/components/MembershipNav.js
@@ -3,16 +3,26 @@ import { NavLink, Outlet } from 'react-router-dom'
 import logo from '../images/moustache-man.jpg'
 
 function MembershipNav({ memberships }) {
+  const activeStyle = { backgroundColor: 'whitesmoke' }
   return (
     <div className='membership-nav'>
       <nav>
         <h3>Chatter engaged in:</h3>
         {
           memberships.map(({id, name, image_url}) => {
-            return <NavLink className='membership-nav-button' key={id} to={`/chatrooms/${id}`}>
-              <img src={image_url || logo} alt={image_url === '' ? 'moustache man' : 'chatroom image'} />
-              <h4>{name}</h4>
-            </NavLink>
+            return (
+              <NavLink 
+                className='membership-nav-button' 
+                key={id} 
+                to={`/chatrooms/${id}`}
+                style={({ isActive }) =>
+                isActive ? activeStyle : undefined
+                }
+                >
+                  <img src={image_url || logo} alt={image_url === '' ? 'moustache man' : 'chatroom image'} />
+                  <h4>{name}</h4>
+              </NavLink>
+            )
           })
         }
       </nav>
