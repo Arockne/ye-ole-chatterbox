572efd047b91a9cc9942b0e910a0530a53c08514
Daniel Calvin <arockne2003@yahoo.com>
2022-01-18 13:08:42 -0700

    change the creator attribute to user of a message

--- app/serializers/communication_serializer.rb
+++ app/serializers/communication_serializer.rb
@@ -1,3 +1,5 @@
 class CommunicationSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :message, :creator
+  attributes :id, :created_at, :message
+
+  belongs_to :user, serializer: UserSerializer
 end
--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -2,13 +2,13 @@ import React from 'react'
 import logo from '../images/moustache-man.jpg'
 
 function Message({ communication }) {
-  const { created_at, message, creator} = communication
+  const { created_at, message, user} = communication
 
   return (
     <div className='message'>
-      <img src={creator.image_url || logo} alt={creator.image_url === '' ? 'moustache man' : 'chatroom image'} />
+      <img src={user.image_url || logo} alt={user.image_url === '' ? 'moustache man' : 'chatroom image'} />
       <div className='content'>
-        <h5><span className='creator'>{creator.username}</span> {created_at}</h5>
+        <h5><span className='creator'>{user.username}</span> {created_at}</h5>
         <p>{message}</p>
       </div>
     </div>
