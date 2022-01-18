005f13cd5ed35d35a914116873b8ec38a3268874
Daniel Calvin <arockne2003@yahoo.com>
2022-01-18 11:35:27 -0700

    include chatroom memberships with response

--- app/serializers/chatroom_show_serializer.rb
+++ app/serializers/chatroom_show_serializer.rb
@@ -1,6 +1,7 @@
 class ChatroomShowSerializer < ActiveModel::Serializer
   attributes :id, :name
 
+  has_many :chatroom_memberships
   has_many :members, serializer: MemberSerializer
   has_many :communications
 end
