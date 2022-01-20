4bd6634edaeb2c2f9a9559fcda4e390b6892bd4c
Daniel Calvin <arockne2003@yahoo.com>
2022-01-20 11:59:25 -0700

    remove unused controller

--- app/controllers/api/hello_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::HelloController < ApplicationController
-  skip_before_action :authorize, only: [:hello_world]
-  
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: { count: session[:count] }, status: :ok
-  end
-end
