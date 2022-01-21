ebf715911efc3d077a12bd7a73576c6b96900f97
Daniel Calvin <arockne2003@yahoo.com>
2022-01-21 09:58:13 -0700

    change css class name of chatroom-search-grid to chatroom-search-list-grid

--- client/src/components/ChatroomSearchList.js
+++ client/src/components/ChatroomSearchList.js
@@ -3,7 +3,7 @@ import ChatroomSearchListItem from './ChatroomSearchListItem'
 
 function ChatroomSearchList({ searchResults, selectedChatroom, setSelectedChatroom }) {
   return (
-    <div className='chatroom-search-grid'>
+    <div className='chatroom-search-list-grid'>
       {
         searchResults.map(chatroom => <ChatroomSearchListItem key={chatroom.id} chatroom={chatroom} selectedChatroom={selectedChatroom} setSelectedChatroom={setSelectedChatroom}/>)
       }
