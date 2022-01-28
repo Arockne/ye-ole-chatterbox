89bbde8aa13f062d9ed27710fde37d8627e0ef0d
Daniel Calvin <arockne2003@yahoo.com>
2022-01-28 11:55:25 -0700

    change format of time

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,9 @@
 class MessageSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :content
 
+  def created_at
+    time = object.created_at
+    time.to_formatted_s(:long_ordinal)
+  end
   belongs_to :user, serializer: UserSerializer
 end
