2079d71911e633e0f7a194d15890a810eed3e3f5
Daniel Calvin <arockne2003@yahoo.com>
2022-01-30 10:27:48 -0700

    implement options for user image

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -9,6 +9,49 @@ const signUpFormDefaultValues = {
   image_url: ''
 }
 
+const profileImages = [
+  {
+    name: 'Peter Paul Rubens',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/Sir_Peter_Paul_Rubens_-_Portrait_of_the_Artist_-_Google_Art_Project.jpg'
+  },
+  {
+    name: 'Frans Hals',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Frans_Hals_-_Portrait_of_a_Man_-_Google_Art_Project_%28579097%29.jpg'
+  },
+  {
+    name: 'Antonio Zucchi',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Angelica_Kaufmann_Antonio_Zucchi.jpg'
+  },
+  {
+    name: 'Sofonisba Anguissola',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Self-portrait_at_the_Easel_Painting_a_Devotional_Panel_by_Sofonisba_Anguissola.jpg'
+  },
+  {
+    name: 'Élisabeth Vigée-Lebrun',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/3/35/Self-portrait_in_a_Straw_Hat_by_Elisabeth-Louise_Vig%C3%A9e-Lebrun.jpg'
+  },
+  {
+    name: 'Ada Lovelace',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Ada_Lovelace_portrait.jpg'
+  },
+  {
+    name: 'Marie Bashkirtseff',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/5/59/Bashkirtseff.jpg'
+  },
+  {
+    name: 'Wolfgang Amadeus Mozart',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Wolfgang-amadeus-mozart_1.jpg'
+  },
+  {
+    name: 'Ludwig van Beethoven',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Beethoven.jpg'
+  },
+  {
+    name: 'Johann Sebastian Bach',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg'
+  }
+]
+
 function SignUpForm({ handleUser }) {
   const [formData, setFormData] = useState(signUpFormDefaultValues)
   const [errors, setErrors] = useState([])
@@ -83,6 +126,12 @@ function SignUpForm({ handleUser }) {
           <br />
           <select name='image_url' value={formData.image_url} onChange={handleFormChange}>
             <option value=''>Default</option>
+            {
+              profileImages.map(({ name, image_url }) => (
+                  <option key={name} value={image_url}>{name}</option>
+                )
+              )
+            }
           </select>
           <br />
           <img src={formData.image_url ||  logo} alt='default profile face'/>
