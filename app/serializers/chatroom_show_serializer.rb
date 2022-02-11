f332c862aaa5b9b6bbd4f139e527b3ed8cb33c71
Daniel Calvin <arockne2003@yahoo.com>
2022-02-11 09:55:08 -0700

    implement current_member

--- app/serializers/chatroom_show_serializer.rb
+++ app/serializers/chatroom_show_serializer.rb
@@ -1,7 +1,12 @@
 class ChatroomShowSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :current_member
 
   has_many :chatroom_memberships
   has_many :members, serializer: MemberSerializer
   has_many :messages
+
+  def current_member
+    object.chatroom_memberships.find(current_user.id)
+  end
+
 end
