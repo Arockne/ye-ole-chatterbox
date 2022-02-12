0160df220bc927120e7c5b056d0489c6ee299c61
Daniel Calvin <arockne2003@yahoo.com>
2022-02-12 14:47:11 -0700

    move welcome image styling into img-4 class

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,7 +4,7 @@ import logo from '../images/tophatmonocle-hat-transparent.png'
 function Welcome({ user: {image_url, username} }) {
   return (
     <div className='welcome'>
-      <img src={image_url || logo} alt={image_url === '' ? 'moustache man' : 'chatroom image'} />
+      <img className='img-4' src={image_url || logo} alt={image_url === '' ? 'moustache man' : 'chatroom image'} />
       <p>Welcome back to the chatter, <br/> <span className='username'>{username}</span></p>
     </div>
   )
