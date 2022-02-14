0e3162ba19bfd1d531963e9e571778fba377bd1c
Daniel Calvin <arockne2003@yahoo.com>
2022-02-14 15:59:18 -0700

    implement authorization on membership deletion

--- app/controllers/api/chatroom_memberships_controller.rb
+++ app/controllers/api/chatroom_memberships_controller.rb
@@ -1,5 +1,7 @@
 class Api::ChatroomMembershipsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  before_action :authorize_chatroom_membership, only: [:destroy]
+
 
   def create
     chatroom = Chatroom.find(params[:chatroom_id])
@@ -8,13 +10,20 @@ class Api::ChatroomMembershipsController < ApplicationController
   end
 
   def destroy
-    membership = ChatroomMembership.find(params[:id])
     membership.destroy
     chatroom = Chatroom.find(params[:chatroom_id])
     render json: chatroom, status: :ok
   end
 
   private
+
+  def membership
+    ChatroomMembership.find(params[:id])
+  end
+
+  def authorize_chatroom_membership
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless membership.user_id == current_user.id
+  end
 
   def render_not_found
     render json: { errors: ['Please select a chatroom from the available choices']}, status: :not_found
