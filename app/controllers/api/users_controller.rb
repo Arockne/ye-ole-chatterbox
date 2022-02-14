a9aa9accca0bfcfa03745ec2864fa859bacb5d42
Daniel Calvin <arockne2003@yahoo.com>
2022-02-13 19:39:27 -0700

    configure user update route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,9 +11,22 @@ class Api::UsersController < ApplicationController
     render json: user, status: :created
   end
 
+  def update
+    if (current_user.id == params[:id].to_i)
+      current_user.update!(update_user_params)
+      render json: current_user, status: :ok
+    else
+      render json: { errors: ["Not authorized"] }, status: :unauthorized
+    end
+  end
+
   private
 
   def user_params
     params.permit(:username, :password, :password_confirmation, :image_url)
+  end
+
+  def update_user_params
+    params.permit(:image_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
       resources :messages, only: [:create, :update]
       resources :chatroom_memberships, only: [:create, :destroy]
     end
-
+    
+    resources :users, only: [:update]
     resources :messages, only: [:show, :destroy]
   end
 
