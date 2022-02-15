e038bc4055c928e83a422b6186d59fc2f86e7111
Daniel Calvin <arockne2003@yahoo.com>
2022-02-15 09:12:57 -0700

    render buttons depending on window size

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react'
 import { Link, NavLink, Outlet, useNavigate, useLocation } from 'react-router-dom'
 import logo from '../images/tophatmonocle-hat-transparent.png'
 
 function Header({ handleUser, user }) {
   const [activeMenu, setActiveMenu] = useState(false)
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0})
   const dropdown = useRef(null)
   const navigate = useNavigate()
   const {pathname} = useLocation()
@@ -15,6 +16,15 @@ function Header({ handleUser, user }) {
       document.removeEventListener("click", handleActiveMenuReset, true);
     }
   }, [])
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
 
   function handleLogout() {
     fetch('/api/logout', {
@@ -38,6 +48,8 @@ function Header({ handleUser, user }) {
     }
   };
 
+  console.log(windowSize)
+
   return (
     <>
       <header className='main-header'>
@@ -46,31 +58,55 @@ function Header({ handleUser, user }) {
           <img className='img-3' src={logo} alt='moustache man' /> 
         </Link>
         <nav className='main-header-nav'>
-          <NavLink 
-            className='nav-button' 
-            to='/chatrooms'
-            style={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }
-            end={pathname === '/chatrooms/search'}
-          >
-            Memberships
-          </NavLink>
-          <NavLink 
-            className='nav-button' 
-            to='/chatrooms/search'
-            style={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }
-          >
-            Parlor Room Exploration
-          </NavLink>
+          {
+            windowSize.width > 1000 ? (
+              <>
+                <NavLink 
+                  className='nav-button' 
+                  to='/chatrooms'
+                  style={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                  }
+                  end={pathname === '/chatrooms/search'}
+                >
+                  Memberships
+                </NavLink>
+                <NavLink 
+                  className='nav-button' 
+                  to='/chatrooms/search'
+                  style={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                  }
+                >
+                  Parlor Room Exploration
+                </NavLink>
+              </>
+            ) : ''
+          }
           <div onClick={handleActiveMenu} ref={dropdown}>
             <div>
               <img className='img-5 pointer' src={user.image_url} alt='profile'/>
               <span className={activeMenu ? 'rotate' : ''}>◁</span>
             </div>
             <nav className={activeMenu ? 'dropdown-menu-active' : 'hidden'}>
+              {
+                windowSize.width <= 1000 ? (
+                  <>
+                    <Link 
+                      className='dropdown-menu-nav-button' 
+                      to='/chatrooms'
+                    >
+                    Memberships
+                    </Link>
+                    <Link 
+                      className='dropdown-menu-nav-button' 
+                      to='/chatrooms/search'
+                    >
+                    Parlor Room Exploration
+                    </Link>
+                  </>
+                ) : ''
+              }
               <Link className='dropdown-menu-nav-button' to='/profile'>Profile</Link>
               <button className='dropdown-menu-nav-button' onClick={handleLogout}>Logout</button>
             </nav>
