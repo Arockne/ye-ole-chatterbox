172833afb26a5ac5a0191291198f8bfa09116590
Daniel Calvin <arockne2003@yahoo.com>
2022-08-11 14:40:40 -0700

    add url back in to consumer

--- client/src/components/Chatroom.js
+++ client/src/components/Chatroom.js
@@ -24,7 +24,8 @@ function Chatroom({ handleChatroomMembershipWithdrawal }) {
       if (chatConnection.consumer) {
         chatConnection.unsubscribe();
       }
-      const consumer = Cable.createConsumer()
+      const url = process.env.NODE_ENV === 'development' ? `ws://${window.location.hostname}:3000/cable` : undefined
+      const consumer = Cable.createConsumer(url)
       const subscription = consumer.subscriptions.create(
         { 
           channel: 'ChatChannel',
